da3ef460dee2ae0646a1cd5de5dd5609807047c0
Deep Bn <deepBn9@gmail.com>
2018-03-05 18:54:29 +0530

    data persistance

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp-camp");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var campgrounds = [{
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("campground", campgroundSchema);
+
+/* Campground.create({
+    name: "Boston Boop",
+    image: "https://images.unsplash.com/photo-1511701455363-d46ed8e3b728?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d65813b9b3ed6a869d4895f0282988a3&auto=format&fit=crop&w=1950&q=80"
+}, function (err, campground) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Campground created: ");
+        console.log(campground);
+    }
+}); */
+
+/* var campgrounds = [{
         name: "Salomon Creek",
         image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c85daa025ee04c951b6ac12fe3ba031a&auto=format&fit=crop&w=1950&q=80"
     },
@@ -39,15 +60,21 @@ var campgrounds = [{
         name: "Boston Boop",
         image: "https://images.unsplash.com/photo-1511701455363-d46ed8e3b728?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d65813b9b3ed6a869d4895f0282988a3&auto=format&fit=crop&w=1950&q=80"
     }
-];
+]; */
 
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {
-        campgrounds: campgrounds
+    Campground.find({}, function (err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {
+                campgrounds: campgrounds
+            });
+        }
     });
 });
 
@@ -62,9 +89,13 @@ app.post("/campgrounds", function (req, res) {
         name: name,
         image: image
     };
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.listen("3000", function () {
