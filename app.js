e6a00604ae3a32a552a56efa88a2a7530c815f92
Deep Bn <deepBn9@gmail.com>
2018-03-10 20:39:49 +0530

    added comments and styled the show page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var SeedDB = require("./seeds");
 SeedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -87,13 +88,13 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
-    Campground.findById(req.params.id, function(err, campground) {
-        if(err) {
+app.post("/campgrounds/:id/comments", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
             console.log(err);
         } else {
-            Comment.create(req.body.comment, function(err, comment) {
-                if(err) {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
                     console.log(err);
                 } else {
                     campground.comments.push(comment);
