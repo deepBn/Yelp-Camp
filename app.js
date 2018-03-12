b2fb6f2cf6972c1c7b546ae659c40be654c940af
Deep Bn <deepBn9@gmail.com>
2018-03-12 11:15:45 +0530

    added login and logout

--- app.js
+++ app.js
@@ -125,22 +125,38 @@ app.post("/campgrounds/:id/comments", function (req, res) {
 // AUTH ROUTES
 //=========================
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user) {
-        if(err) {
+app.post("/register", function (req, res) {
+    var newUser = new User({
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             return res.render("register");
         } else {
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/campgrounds");
             });
         }
     })
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function (req, res) {});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
 });
 
 //START SERVER
