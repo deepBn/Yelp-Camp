6c602926d2c644279e0d9c92607828ca92249c6f
Deep Bn <deepBn9@gmail.com>
2018-03-13 19:01:44 +0530

    added edit and update route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
 var User = require("./models/user");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
@@ -21,6 +22,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(methodOverride("_method"));
 
 //AUTH CONFIG
 app.use(require("express-session")({
