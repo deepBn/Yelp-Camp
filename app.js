7f7e8acf1828663b8cdfa9dcb45a69b1b7e0868e
Deep Bn <deepBn9@gmail.com>
2018-03-15 20:36:42 +0530

    some UI changes and added PORT option for heroku

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 //START SERVER
-app.listen("3000", function () {
+app.listen(process.env.PORT||8080, function () {
     console.log("YelpCamp server is running!");
 });
